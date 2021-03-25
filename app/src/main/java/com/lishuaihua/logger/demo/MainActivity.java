f5077a91db89cae25b42e173cd0d064894804615
lish <850716183@qq.com>
2021-03-26 00:08:03 +0800

    Update MainActivity.java

--- app/src/main/java/com/lishuaihua/logger/demo/MainActivity.java
+++ app/src/main/java/com/lishuaihua/logger/demo/MainActivity.java
@@ -9,7 +9,9 @@ import com.lishuaihua.logger.JackLogger;
 import com.lishuaihua.logger.demo.R;
 
 public class MainActivity extends AppCompatActivity {
-private static final String TAG=MainActivity.class.getSimpleName();
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,95 +19,38 @@ private static final String TAG=MainActivity.class.getSimpleName();
     }
 
     public void logV(View view) {
-        JackLogger.v(TAG,"Verbose");
+        JackLogger.v(TAG, "Verbose");
     }
 
     public void logD(View view) {
-        JackLogger.d(TAG,"Debug");
+        JackLogger.d(TAG, "Debug");
     }
+
     public void logI(View view) {
-        JackLogger.i(TAG,"Info");
+        JackLogger.i(TAG, "Info");
     }
+
     public void logE(View view) {
-        JackLogger.e(TAG,"ERROR");
+        JackLogger.e(TAG, "ERROR");
     }
 
     public void logW(View view) {
-        JackLogger.w(TAG,"Warn");
+        JackLogger.w(TAG, "Warn");
     }
 
     public void logJson(View view) {
-       String json="{\"tagValue\":\"满150-15；满300-40\",\"tagType\":2}";
-        JackLogger.json(TAG,json);
+        String json = "{\"tagValue\":\"满150-15；满300-40\",\"tagType\":2}";
+        JackLogger.json(TAG, json);
     }
 
     public void logXml(View view) {
-      String xml="<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
-              "<LinearLayout xmlns:android=\"http://schemas.android.com/apk/res/android\"\n" +
-              "    xmlns:app=\"http://schemas.android.com/apk/res-auto\"\n" +
-              "    xmlns:tools=\"http://schemas.android.com/tools\"\n" +
-              "    android:layout_width=\"match_parent\"\n" +
-              "    android:layout_height=\"match_parent\"\n" +
-              "    android:orientation=\"vertical\"\n" +
-              "    tools:context=\".MainActivity\">\n" +
-              "\n" +
-              "    <Button\n" +
-              "        android:layout_width=\"match_parent\"\n" +
-              "        android:layout_height=\"50dp\"\n" +
-              "        android:onClick=\"logV\"\n" +
-              "        android:padding=\"10dp\"\n" +
-              "        android:text=\"JackLogger.v\"\n" +
-              "        android:textSize=\"14sp\"></Button>\n" +
-              "\n" +
-              "    <Button\n" +
-              "        android:layout_width=\"match_parent\"\n" +
-              "        android:layout_height=\"50dp\"\n" +
-              "        android:onClick=\"logD\"\n" +
-              "        android:padding=\"10dp\"\n" +
-              "        android:text=\"JackLogger.d\"\n" +
-              "        android:textSize=\"14sp\"></Button>\n" +
-              "\n" +
-              "    <Button\n" +
-              "        android:layout_width=\"match_parent\"\n" +
-              "        android:layout_height=\"50dp\"\n" +
-              "        android:onClick=\"logI\"\n" +
-              "        android:padding=\"10dp\"\n" +
-              "        android:text=\"JackLogger.i\"\n" +
-              "        android:textSize=\"14sp\"></Button>\n" +
-              "\n" +
-              "    <Button\n" +
-              "        android:layout_width=\"match_parent\"\n" +
-              "        android:layout_height=\"50dp\"\n" +
-              "        android:onClick=\"logE\"\n" +
-              "        android:padding=\"10dp\"\n" +
-              "        android:text=\"JackLogger.e\"\n" +
-              "        android:textSize=\"14sp\"></Button>\n" +
-              "\n" +
-              "    <Button\n" +
-              "        android:layout_width=\"match_parent\"\n" +
-              "        android:layout_height=\"50dp\"\n" +
-              "        android:onClick=\"logW\"\n" +
-              "        android:padding=\"10dp\"\n" +
-              "        android:text=\"JackLogger.w\"\n" +
-              "        android:textSize=\"14sp\"></Button>\n" +
-              "\n" +
-              "    <Button\n" +
-              "        android:layout_width=\"match_parent\"\n" +
-              "        android:layout_height=\"50dp\"\n" +
-              "        android:onClick=\"logJson\"\n" +
-              "        android:padding=\"10dp\"\n" +
-              "        android:text=\"JackLogger.json\"\n" +
-              "        android:textSize=\"14sp\"></Button>\n" +
-              "\n" +
-              "    <Button\n" +
-              "        android:layout_width=\"match_parent\"\n" +
-              "        android:layout_height=\"50dp\"\n" +
-              "        android:onClick=\"logXml\"\n" +
-              "        android:padding=\"10dp\"\n" +
-              "        android:text=\"JackLogger.xml\"\n" +
-              "        android:textSize=\"14sp\"></Button>\n" +
-              "\n" +
-              "</LinearLayout>";
-        JackLogger.xml(TAG,xml);
+        String xml = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
+                "<LinearLayout xmlns:android=\"http://schemas.android.com/apk/res/android\"\n" +
+                "android:layout_width=\"match_parent\"\n" +
+                "android:layout_height=\"match_parent\"\n" +
+                "android:orientation=\"vertical\"\n" +
+                ">\n" +
+                "</LinearLayout>";
+        JackLogger.xml(TAG, xml);
     }
 }
